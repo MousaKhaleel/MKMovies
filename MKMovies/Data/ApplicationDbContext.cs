16d106791152ae30e2172d1df64b30bb61e6fbb8
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-07-26 15:36:41 +0300

    add entity framework and a database

---
+++ MKMovies/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MKMovies.Models;
+
+namespace MKMovies.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ MKMovies/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MKMovies.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(300)]
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
